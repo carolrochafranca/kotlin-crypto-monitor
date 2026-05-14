5b830b9bfd5fe90fcb7a1b86318fc3e532f03580
Caroline Rocha <caroline10francarocha@gmail.com>
2026-05-13 22:28:18 -0300

    Criando o payload de resposta da API aberta do BITCOIN

---
+++ app/src/main/java/com/github/carolrochafranca/kotlin_cryptor_monitor/model/TicketResponse.kt
@@ -0,0 +1,15 @@
+package com.github.carolrochafranca.kotlin_cryptor_monitor.model
+
+class TickerResponse(
+    val ticker: Ticker
+)
+
+class Ticker(
+    val high: String,
+    val low: String,
+    val vol: String,
+    val last: String,
+    val buy: String,
+    val sell: String,
+    val date: Long
+)
