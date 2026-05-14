707b33db655d74f4d3326a4b05972a21c245d6df
Caroline Rocha <caroline10francarocha@gmail.com>
2026-05-13 22:17:42 -0300

    Incluindo a dependência retrofit para comunicação entre API's

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,8 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.activity:activity-ktx:1.7.0")
 }
